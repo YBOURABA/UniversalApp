3663156ed6b2b9dc53c012b08c3077ec47ee9def
Yahya BOURABA <yahya.bouraba@orange.com>
2021-05-31 09:17:07 +0200

    Initial Commit

---
+++ Shared/UniversalAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalAppTestApp.swift
+//  Shared
+//
+//  Created by Yahya BOURABA on 31/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct UniversalAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
